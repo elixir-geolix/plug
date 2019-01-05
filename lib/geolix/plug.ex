20c9d9681b9dbb18d6809db3dd6e496566815c71
Marc Neudert <marc.neudert@gmail.com>
2019-01-05 13:32:16 +0100

    doc

--- lib/geolix/plug.ex
+++ lib/geolix/plug.ex
@@ -1,6 +1,36 @@
 defmodule Geolix.Plug do
   @moduledoc """
   Geolix Plug
+
+  ## Usage
+
+  After ensuring `:geolix` is configured you need to add the plug:
+
+      defmodule MyRouter do
+        use Plug.Router
+
+        # ...
+        plug Geolix.Plug, where: :database_id
+        # ...
+
+        plug :match
+        plug :dispatch
+      end
+
+  Depending on how you are using plugs the actual location may vary.
+  Please consult your frameworks documentation to find the proper place.
+
+  Once setup the connection will be automatically enriched with the results of
+  a lookup based on the connections `:remote_ip`:
+
+      defmodule MyRouter do
+        get "/" do
+          case Geolix.Plug.get_result(conn) do
+            nil -> send_resp(404, "No data or lookup failed")
+            lookup -> send_resp(200, "Client country: #{lookup.country.name}")
+          end
+        end
+      end
   """
 
   import Plug.Conn
