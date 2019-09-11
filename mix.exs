fd774569cf9abf2a32aab9eb3eeeac11e5b7c1ce
Marc Neudert <marc.neudert@gmail.com>
2019-09-11 18:57:51 +0200

    Bump version to v0.2.0-dev

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Geolix.Plug.MixProject do
     [
       app: :geolix_plug,
       name: "Geolix Plug",
-      version: "0.1.0",
+      version: "0.2.0-dev",
       elixir: "~> 1.5",
       deps: deps(),
       description: "Geolix Plug",
@@ -48,7 +48,7 @@ defmodule Geolix.Plug.MixProject do
   defp docs do
     [
       main: "Geolix.Plug",
-      source_ref: "v0.1.0",
+      source_ref: "master",
       source_url: @url_github
     ]
   end
