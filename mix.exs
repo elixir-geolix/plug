e922732109739272bda1e9d65e2b3d7e865374a7
Marc Neudert <marc.neudert@gmail.com>
2020-05-12 16:54:31 +0200

    Loosen :dialyxir requirement

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Geolix.Plug.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12.0", only: :test},
       {:geolix, "~> 1.0"},
